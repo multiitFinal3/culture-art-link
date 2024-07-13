89ea9d7c07a060114eab1cbe5e11cc6625991e0e
ThebeginnerJiyeon <jyu35467@gmail.com>
2024-07-13 15:19:51 +0900

    1
    1

---
+++ src/test/java/com/multi/culture_link/CultureLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.culture_link;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CultureLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
