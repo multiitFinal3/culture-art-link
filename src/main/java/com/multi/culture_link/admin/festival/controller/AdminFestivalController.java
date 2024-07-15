989a97b6645381bdfdfca4ba8470a75676a823de
ThebeginnerJiyeon <jyu35467@gmail.com>
2024-07-16 00:35:01 +0900

    feat admin page layout 초안 완성
    초안 완성

---
+++ src/main/java/com/multi/culture_link/admin/festival/controller/AdminFestivalController.java
@@ -0,0 +1,23 @@
+package com.multi.culture_link.admin.festival.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/festival-regulate")
+public class AdminFestivalController {
+	
+	
+	@GetMapping
+	public String festivalManage(){
+		
+		
+		return "/admin/festival/festivalRegulate";
+		
+	}
+	
+	
+	
+}
