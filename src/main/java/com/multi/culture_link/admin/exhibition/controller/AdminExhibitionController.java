56643ccad2693f6220e4ba4579462f289c934aeb
Hyeseon Ann <119028812+harin0224@users.noreply.github.com>
2024-07-16 23:22:18 +0900

    api test

---
+++ src/main/java/com/multi/culture_link/admin/exhibition/controller/AdminExhibitionController.java
@@ -0,0 +1,24 @@
+package com.multi.culture_link.admin.exhibition.controller;
+
+
+import com.multi.culture_link.admin.exhibition.service.ExhibitionApiService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/admin/exhibition-regulate")
+public class AdminExhibitionController {
+    private final ExhibitionApiService exhibitionApiService;
+
+    @GetMapping
+    public String exhibitionManage(){
+        exhibitionApiService.getApiResponse();
+
+        return "/admin/exhibition/exhibitionRegulate";
+
+    }
+
+}
---
+++ src/main/java/com/multi/culture_link/admin/exhibition/model/dto/ExhibitionApiDto.java
@@ -0,0 +1,22 @@
+package com.multi.culture_link.admin.exhibition.model.dto;
+
+import lombok.Data;
+
+@Data
+public class ExhibitionApiDto {
+    @Data
+    static class exhibition{
+        public int id;
+        public String title;
+        public String artist;
+        public String museum;
+        public String start_date;
+        public String end_date;
+        public int price;
+        public String image;
+        public String description;
+        public String sub_description;
+        public String url;
+    }
+
+}
