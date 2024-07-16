a0a33bbd2bd0be4d10774998ca8c69e38b7c5c78
ThebeginnerJiyeon <jyu35467@gmail.com>
2024-07-17 00:52:55 +0900

    feat  시큐리티 로그인 기능 완성

--- src/main/java/com/multi/culture_link/config/SpringSecurityConfiguration.java
+++ src/main/java/com/multi/culture_link/config/SpringSecurityConfiguration.java
@@ -17,51 +17,39 @@ public class SpringSecurityConfiguration {
 		return new BCryptPasswordEncoder();
 	}
 	
-
-	
-	
 	
 	// 추후에 시간 남으면 permit list 테이블 만들 예정 및 에러 처리할 예정
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 		
-		httpSecurity.authorizeHttpRequests((auth)->auth
-		
-				.requestMatchers("/","/home","/user/login","/user/logout","/user/signUp").permitAll()
+		httpSecurity.authorizeHttpRequests((auth) -> auth
+				
+				/*.requestMatchers("/","/home","/user/login","/user/logout","/user/signUp","/user/login2").permitAll()
 				.requestMatchers("/admin").hasRole("ADMIN")
 				.requestMatchers("/user/myPage").hasAnyRole("ADMIN", "USERS")
-				.anyRequest().authenticated()
+				.anyRequest().authenticated().*/.anyRequest().permitAll()
 		
 		);
 		
 		
-		httpSecurity.formLogin((auth) -> auth.loginPage("/user/login").loginProcessingUrl("/user/login2").permitAll());
+		/*httpSecurity.formLogin((auth) -> auth.loginPage("/user/login").loginProcessingUrl("/user/login2").permitAll());*/
+		httpSecurity.formLogin((auth) -> auth.loginPage("/user/login").loginProcessingUrl("/user/login2").defaultSuccessUrl("/", true));
 		
 		
-		
-		httpSecurity.logout((auth)->auth.logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+		httpSecurity.logout((auth) -> auth.logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
 				.invalidateHttpSession(true)
 				.deleteCookies("JSESSIONID")
 				.logoutSuccessUrl("/")
 		
 		);
 		
-		httpSecurity.csrf((auth)->auth.disable());
-		
-		
-		
-		
+		httpSecurity.csrf((auth) -> auth.disable());
 		
 		
 		return httpSecurity.build();
 		
-	
-	
-	
-	
 		
 	}
 	
-
-
+	
 }
