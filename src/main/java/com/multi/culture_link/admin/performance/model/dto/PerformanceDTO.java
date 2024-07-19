431519c365e1750fd686e263a641b03e43c95247
정다영 <162556877+jungday5ung@users.noreply.github.com>
2024-07-19 14:22:51 +0900

    api data update!

---
+++ src/main/java/com/multi/culture_link/admin/performance/model/dto/PerformanceDTO.java
@@ -0,0 +1,16 @@
+package com.multi.culture_link.admin.performance.model.dto;
+
+import lombok.Data;
+
+@Data
+public class PerformanceDTO {
+    private String id;
+    private String title;
+    private String state;
+    private String startDate;
+    private String endDate;
+    private String venue;
+    private String poster;
+    private String genre;
+    private String openRun;
+}
