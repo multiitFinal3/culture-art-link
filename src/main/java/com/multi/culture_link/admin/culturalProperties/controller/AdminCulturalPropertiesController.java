c3660a5746f74e7ff44f7d2f86a0465bacb61321
hyewon <rqzx00@gmail.com>
2024-07-23 16:42:16 +0900

    test

--- src/main/java/com/multi/culture_link/admin/culturalProperties/controller/AdminCulturalPropertiesController.java
+++ src/main/java/com/multi/culture_link/admin/culturalProperties/controller/AdminCulturalPropertiesController.java
@@ -1,14 +1,17 @@
 package com.multi.culture_link.admin.culturalProperties.controller;
 
 
-import com.multi.culture_link.admin.culturalProperties.dto.CulturalPropertiesDTO;
+import com.multi.culture_link.admin.culturalProperties.model.dto.CulturalPropertiesDTO;
 import com.multi.culture_link.admin.culturalProperties.service.AdminCulturalPropertiesService;
+import com.multi.culture_link.admin.exhibition.model.dto.api.ExhibitionApiDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 
 @Controller
 @RequestMapping("/admin/cultural-properties-regulate")
@@ -35,19 +38,19 @@ public class AdminCulturalPropertiesController {
 
 	@PostMapping("/fetchApiData")
 	@ResponseBody
-	public List<CulturalPropertiesDTO> culturalPropertiesList(@RequestParam("page") int page, Model model) {
+	public List<CulturalPropertiesDTO> culturalPropertiesList(@RequestParam("pageIndex") int pageIndex, Model model) {
 
 		System.out.println("list확인");
-		List<CulturalPropertiesDTO> list = adminCulturalPropertiesService.fetchApiData(page);
+		List<CulturalPropertiesDTO> list = adminCulturalPropertiesService.fetchApiData(pageIndex);
 		model.addAttribute("culturalPropertiesList", list);
-		try {
-			list = adminCulturalPropertiesService.fetchApiData(page);
-
-
-
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+//		try {
+//			list = adminCulturalPropertiesService.fetchApiData(pageIndex);
+//
+//
+//
+//		} catch (Exception e) {
+//			throw new RuntimeException(e);
+//		}
 
 
 		System.out.println("list : " + list);
@@ -56,6 +59,47 @@ public class AdminCulturalPropertiesController {
 
 
 	}
+
+//	@PostMapping("/insert")
+//	@ResponseBody
+//	public String insertDB(@RequestBody List<String> checkedItems) {
+//		try {
+//			for (String index : checkedItems) {
+//				// index를 사용하여 CulturalPropertiesDTO 생성
+//				CulturalPropertiesDTO dto = new CulturalPropertiesDTO();
+//				dto.setId(Integer.parseInt(index)); // 예시로 id 설정
+//
+//				// 필요한 경우 다른 필드도 설정 가능
+//
+//				// CulturalPropertiesDTO를 사용하여 DB에 추가
+//				adminCulturalPropertiesService.insertDB(dto);
+//			}
+//			return "문화재가 DB에 성공적으로 추가되었습니다.";
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			return "DB 추가 중 오류가 발생했습니다.";
+//		}
+//	}
+
+	@PostMapping("/insert")
+	public ResponseEntity<String> insertDB(@RequestBody List<CulturalPropertiesDTO> checkedItem){
+//		for (String index : checkedItem) {
+//			CulturalPropertiesDTO dto = new CulturalPropertiesDTO();
+//			dto.setId(Integer.parseInt(index));
+			adminCulturalPropertiesService.insertDB(checkedItem);
+//		}
+		return ResponseEntity.ok("문화재가 DB에 성공적으로 추가되었습니다.");
+	}
+
+
+	@GetMapping("/getDBData")
+	public List<CulturalPropertiesDTO> selectDB() {
+
+		return adminCulturalPropertiesService.selectDB(); // 모든 문화재 데이터 가져오기
+	}
+
+
+
 
 
 
