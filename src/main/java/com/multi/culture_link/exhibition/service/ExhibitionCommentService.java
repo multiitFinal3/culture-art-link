a5a6d041b86d3af3112f2620c05b574017c80c15
ThebeginnerJiyeon <jyu35467@gmail.com>
2024-08-07 17:27:43 +0900

    fix 전시 별점을 별 모양으로 바꾸고 테이블 UI 수정

--- src/main/java/com/multi/culture_link/exhibition/service/ExhibitionCommentService.java
+++ src/main/java/com/multi/culture_link/exhibition/service/ExhibitionCommentService.java
@@ -1,39 +1,36 @@
 package com.multi.culture_link.exhibition.service;
 
-import com.multi.culture_link.admin.exhibition.model.dto.api.ExhibitionApiDto;
 import com.multi.culture_link.exhibition.model.dao.ExhibitionCommentDao;
-import com.multi.culture_link.exhibition.model.dao.ExhibitionDao;
 import com.multi.culture_link.exhibition.model.dto.ExhibitionCommentDto;
-import com.multi.culture_link.exhibition.model.dto.ExhibitionDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class ExhibitionCommentService {
-    private final ExhibitionCommentDao exhibitionCommentDao;
-
-    public List<ExhibitionCommentDto> getComment(ExhibitionCommentDto data) {
-        return exhibitionCommentDao.getComment(data);
-    }
-
-    public void createComment(ExhibitionCommentDto data) {
-        exhibitionCommentDao.createComment(data);
-    }
-
-    public void deleteComment(ExhibitionCommentDto data) {
-        exhibitionCommentDao.deleteComment(data);
-    }
-    public double getAverageRating(int exhibitionId){
-        return exhibitionCommentDao.getAverageRating(exhibitionId);
-    };
-
-    public List<ExhibitionCommentDto> getUserComment(int userId) {
-        return exhibitionCommentDao.getUserComment(userId);
-    }
+	private final ExhibitionCommentDao exhibitionCommentDao;
+	
+	public List<ExhibitionCommentDto> getComment(ExhibitionCommentDto data) {
+		return exhibitionCommentDao.getComment(data);
+	}
+	
+	public void createComment(ExhibitionCommentDto data) {
+		exhibitionCommentDao.createComment(data);
+	}
+	
+	public void deleteComment(ExhibitionCommentDto data) {
+		exhibitionCommentDao.deleteComment(data);
+	}
+	
+	public double getAverageRating(int exhibitionId) {
+		return exhibitionCommentDao.getAverageRating(exhibitionId);
+	}
+	
+	public List<ExhibitionCommentDto> getUserComment(int userId) {
+		return exhibitionCommentDao.getUserComment(userId);
+	}
 }
