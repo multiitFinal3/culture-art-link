39c7109fdf7bb2aae59c6ececa7306f5812d0171
Hyeseon Ann <119028812+harin0224@users.noreply.github.com>
2024-08-19 16:05:42 +0900

    recommend func

--- src/main/java/com/multi/culture_link/exhibition/model/dao/ExhibitionDao.java
+++ src/main/java/com/multi/culture_link/exhibition/model/dao/ExhibitionDao.java
@@ -11,20 +11,26 @@ import java.util.Map;
 public interface ExhibitionDao {
 
     List<ExhibitionApiDto> searchExhibition(Map<String, String> searchParams);
+
     ExhibitionDto getExhibitionById(int userId, int exhibitionId);
+
     // 찜, 관심없음 변동
     void setInterested(int userId, int exhibitionId, String state);
+
     // 전시 리스트 관심 연동하여 가져오기
     List<ExhibitionDto> getExhibition(int userId);
-    
+
     // 찜, 관심없음 리스트
 //    List<ExhibitionApiDto> getUserInterestedExhibitions(int id);
 
     List<ExhibitionApiDto> getLikeExhibition(int userId);
+
     List<ExhibitionApiDto> getUnlikeExhibition(int userId);
+
     String getInterestState(int userId, int exhibitionId);
 
     // 관심 상태 제거
     void removeInterest(int userId, int exhibitionId);
-
 }
+
+
