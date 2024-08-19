65fe799ed22f0fac54845ca971c1388ff3f657a5
hyewon <rqzx00@gmail.com>
2024-08-19 09:44:42 +0900

    1

--- src/main/java/com/multi/culture_link/common/keyword/service/GPTAssistance.java
+++ src/main/java/com/multi/culture_link/common/keyword/service/GPTAssistance.java
@@ -12,7 +12,7 @@ public interface GPTAssistance {
 //	@SystemMessage("문화재 이름이랑 지역 제외한 핵심 키워드 5개를 뽑아 ,로 이어서 반환")
 //	String chat2(@UserMessage String userMessage);
 
-	@SystemMessage("핵심 키워드 5개를 문화재 이름이랑 지역 빼고 뽑아 ,로 이어서 반환")
+	@SystemMessage("핵심 키워드 5개를 문화재 이름이랑 지역 빼고 중복없이 뽑아 ,로 이어서 반환")
 	String chat2(@UserMessage String userMessage);
 
 
